cb0a6fb6796dd3d90f5f959b52aae357a9b3b823
Caner Uçar <caner.ucar@akinon.com>
2021-04-03 21:05:34 +0300

    Add Auth0 login logout

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/global.css';
 import Header from '../components/header';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="antialiased text-gray-700">
-    <Header></Header>
-      <main className="my-6">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain="canerucar.us.auth0.com"
+      clientId="4K5RR6211QF0ztz0vouqaAy3zk7o4Nf6"
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className="antialiased text-gray-700">
+        <Header></Header>
+        <main className="my-6">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
   ); 
 }
 
