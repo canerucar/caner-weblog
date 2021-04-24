7333430957d060b54934a84540fae3c776f8a24e
Caner Uçar <caner.ucar@akinon.com>
2021-04-24 14:07:47 +0300

    Add AuthId and Redis POST/GET

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 function MyApp({ Component, pageProps }) {
   return (
     <Auth0Provider
-      domain="canerucar.us.auth0.com"
-      clientId="4K5RR6211QF0ztz0vouqaAy3zk7o4Nf6"
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_URL_AUTH0_CLIENT_ID}
       redirectUri={process.env.NEXT_PUBLIC_URL}
     >
       <div className="antialiased text-gray-700">
